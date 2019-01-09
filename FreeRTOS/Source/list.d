8a0f3383067c0fea429c3d7e82b0f2bee54d5d2e
Marek Materzok <tilk@tilk.eu>
2019-01-09 14:47:57 +0100

    Cleanup.

--- FreeRTOS/Source/list.d
+++
@@ -1,6 +0,0 @@
-list.o list.d : FreeRTOS/Source/list.c FreeRTOS/Source/include/FreeRTOS.h \
- FreeRTOSConfig.h FreeRTOS/Source/include/projdefs.h \
- FreeRTOS/Source/include/portable.h \
- FreeRTOS/Source/include/deprecated_definitions.h \
- FreeRTOS/Source/include/../portable/GCC/ATMega323/portmacro.h \
- FreeRTOS/Source/include/mpu_wrappers.h FreeRTOS/Source/include/list.h
